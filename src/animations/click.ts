e963e2e6d7d846060674da2564bb00314b5d3f45
David Baker <dbaker@virtual-affairs.com>
2019-06-27 17:46:38 +0200

    add callbacks and click from cursor animations

--- src/animations/click.ts
+++ src/animations/click.ts
@@ -4,6 +4,8 @@ import { Burst, Timeline } from 'mo-js';
 import { cssConstants as css } from '../shared/constants';
 
 const burstEffect: any = new Burst({
+  left: 0,
+  top: 0,
   radius: { 0: 100 },
   count: 20,
   children: {
@@ -11,12 +13,14 @@ const burstEffect: any = new Burst({
     stroke: css.colors.colorRed,
     strokeWidth: { 6: 0 },
     angle: { 360: 0 },
-    duration: 3000,
+    duration: 1000,
     radius: { 30: 5 }
   }
 });
 
 const burstEffect2: any = new Burst({
+  left: 0,
+  top: 0,
   radius: { 0: 360 },
   count: 12,
   children: {
@@ -26,11 +30,19 @@ const burstEffect2: any = new Burst({
     points: 7,
     strokeWidth: { 6: 0 },
     angle: { '-360': 0 },
-    duration: 3000,
+    duration: 800,
     radius: { 30: 5 }
   }
 });
 
-export const burst = new Timeline({
-  repeat: 0
-}).add(burstEffect, burstEffect2);
+export const burst = (e: any, cb: any) => {
+  const burstTime = new Timeline({
+    onPlaybackComplete: () => cb()
+  });
+  return burstTime
+    .add(
+      burstEffect.tune({ x: e.pageX, y: e.pageY }),
+      burstEffect2.tune({ x: e.pageX, y: e.pageY })
+    )
+    .replay();
+};
--- src/welcome/welcome.tsx
+++ src/welcome/welcome.tsx
@@ -1,27 +1,44 @@
 import React from 'react';
 import { RouteComponentProps } from 'react-router';
+// import { Html, Timeline } from 'mo-js';
 
 import { burst } from '../animations/click';
 
 interface welcomeProps extends RouteComponentProps {}
 
-export default class Welcome extends React.Component<welcomeProps> {
+interface State {
+  title: String;
+  timeline?: any;
+}
+
+export default class Welcome extends React.Component<welcomeProps, State> {
   constructor(props: welcomeProps) {
     super(props);
-    console.log(burst);
+    this.state = {
+      title: 'Welcome'
+    };
+    console.log(this);
+  }
+
+  titleBuilder() {
+    return this.state.title.split('').map((letter, index) => {
+      return <span key={index}>{letter}</span>;
+    });
   }
 
   render() {
+    console.log(this.titleBuilder());
+
     return (
       <React.Fragment>
         <h1
           className="clickable"
-          onClick={() => {
-            burst.play();
-            // this.props.history.push("dude")
+          onClick={e => {
+            e.preventDefault();
+            burst(e, () => this.props.history.push('dude'));
           }}
         >
-          Welcome
+          {this.titleBuilder()}
         </h1>
       </React.Fragment>
     );
