b175ef00099cae59c8ee46a703cd20ef6f0780ea
David Baker <dbaker@virtual-affairs.com>
2019-06-27 14:10:31 +0200

    fix prettierignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import React from "react";
-import styles from "./app.module.scss";
-import Routing from "./routing";
-import { BrowserRouter } from "react-router-dom";
-import Loadable from "react-loadable";
-
+import React from 'react';
+import styles from './app.module.scss';
+import Routing from './routing';
+import { BrowserRouter } from 'react-router-dom';
+import Loadable from 'react-loadable';
 
 const LazyStarfield = Loadable({
-  loader: ()=> import("./starfield/starfield"),
-  loading: ()=>null
-})
-
+  loader: () => import('./starfield/starfield'),
+  loading: () => null
+});
 
 const App: React.FC = () => {
   return (
