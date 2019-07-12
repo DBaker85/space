f4a8870b15262144fab485d1ba2a3adc9e997d99
David Baker <dbaker@virtual-affairs.com>
2019-07-12 14:23:08 +0200

    setup for gsap and icon structure

--- src/welcome/welcome.tsx
+++ src/welcome/welcome.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { RouteComponentProps } from 'react-router';
-import { TimelineLite } from 'gsap';
-
+import { TimelineLite, TweenLite } from 'gsap';
+import { MorphSVGPlugin } from '../gsap-bonus';
 import { cssConstants } from '../shared/css-constants';
+
+import GithubIcon from '../icons/github-icon';
+import CodePenIcon from '../icons/codepen-icon';
+import LinkedInIcon from '../icons/linkedin-icon';
+
 import styles from './welcome.module.scss';
 
 interface welcomeProps extends RouteComponentProps {}
@@ -34,31 +39,31 @@ export default class Welcome extends React.Component<welcomeProps, State> {
   }
 
   componentDidMount() {
-    this.welcomeTimeline
-      .staggerFrom(
-        this.myElements,
-        1,
-        {
-          y: function() {
-            return -Math.random() * 600;
-          },
-          x: function() {
-            return Math.random() * 300;
-          },
-          autoAlpha: 0,
-          rotation: function() {
-            return Math.random() * 300;
-          },
-          color: cssConstants.colors.colorRed
-        },
-        0.1,
-        null,
-        () => console.log('complete')
-      )
-      .play();
+    // this.welcomeTimeline
+    //   .staggerFrom(
+    //     this.myElements,
+    //     1,
+    //     {
+    //       y: function() {
+    //         return -Math.random() * 600;
+    //       },
+    //       x: function() {
+    //         return Math.random() * 300;
+    //       },
+    //       autoAlpha: 0,
+    //       rotation: function() {
+    //         return Math.random() * 300;
+    //       },
+    //       color: cssConstants.colors.colorRed
+    //     },
+    //     0.1,
+    //     null,
+    //     () => console.log('phase one complete')
+    //   ).play();
   }
 
   render() {
+    const { title } = this.state;
     return (
       <React.Fragment>
         <h1
@@ -68,8 +73,13 @@ export default class Welcome extends React.Component<welcomeProps, State> {
             e.preventDefault();
           }}
         >
-          {this.titleBuilder()}
+          {title}
         </h1>
+        <div className={styles['social-holder']}>
+          <GithubIcon color={cssConstants.colors.colorWhite} />
+          <CodePenIcon color={cssConstants.colors.colorWhite} />
+          <LinkedInIcon color={cssConstants.colors.colorWhite} />
+        </div>
       </React.Fragment>
     );
   }
