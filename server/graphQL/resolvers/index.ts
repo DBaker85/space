2e98a8f87051ea132f6bbb1258c47d08ffd6054b
David Baker <dbaker@virtual-affairs.com>
2019-09-02 17:32:36 +0200

    adding first query

--- server/graphQL/resolvers/index.ts
+++ server/graphQL/resolvers/index.ts
@@ -1,11 +1,21 @@
-const hello = {
-  hello: () => 'Hello world!'
-};
-const goodbye = {
-  goodbye: () => 'Goodbye cruel world'
+import { ApolloError } from 'apollo-server-express';
+import fetch from 'node-fetch';
+
+const asteroids = {
+  neo: async () => {
+    const response = await fetch(
+      'https://api.nasa.gov/neo/rest/v1/feed/today?detailed=false&api_key=DEMO_KEY'
+    );
+    const json = await response.json();
+    if (json.error) {
+      return new ApolloError(json.error.message);
+    } else {
+      return json;
+    }
+  }
 };
 
 // Provide resolver functions for your schema fields
 export const resolvers = {
-  Query: { ...hello, ...goodbye }
+  Query: { ...asteroids }
 };
