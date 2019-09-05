56ed1e842b850b6bb8202e38f64df3952a9a6d6c
David Baker <dbaker@virtual-affairs.com>
2019-09-05 11:17:55 +0200

    add more tests

--- src/shared/utils/utils.test.ts
+++ src/shared/utils/utils.test.ts
@@ -1,5 +1,9 @@
 import { uid } from './utils';
 
+function checkIfDuplicateExists(w: Array<any>) {
+  return new Set(w).size !== w.length;
+}
+
 it('generates an id of as a string', () => {
   const id = uid(4);
   expect(typeof id).toBe('string');
@@ -10,4 +14,12 @@ it('generates ids of correct length', () => {
   const id2 = uid(20);
   expect(id).toHaveLength(5);
   expect(id2).toHaveLength(20);
+});
+
+it('generates unique ids', () => {
+  const ids = [...new Array(30)].map(() => {
+    return uid(10);
+  });
+  const hasDuplicates = checkIfDuplicateExists(ids);
+  expect(hasDuplicates).toBeFalsy();
 });
