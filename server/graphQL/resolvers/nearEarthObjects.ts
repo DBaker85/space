b22c828476dfd669c3e513ad783b4eb275c68cbc
David Baker <dbaker@virtual-affairs.com>
2019-09-09 15:42:10 +0200

    refactor queries

--- server/graphQL/resolvers/nearEarthObjects.ts
+++ server/graphQL/resolvers/nearEarthObjects.ts
@@ -1,34 +1,73 @@
 import { ApolloError } from 'apollo-server-express';
-import fetch from 'node-fetch';
+import fetch, { Response } from 'node-fetch';
+import { NearEarthObjectList } from '../../models/NasaApis';
+import { ApiErrorResponse } from '../../models/ApiResponses';
+import { mapRange } from '../../utils/utils';
+
+interface ApiResponse extends NearEarthObjectList, ApiErrorResponse {}
+
+interface NearEarthObject {
+  size: number;
+  orbit: number;
+}
+
+interface QueryResponse {
+  elements: number;
+  objects: NearEarthObject[];
+}
+
+const mapSizeRange = (value: number, in_min: number, in_max: number) =>
+  mapRange(value, in_min, in_max, 5, 25);
 
 export const nearEarthObjectsQueries = {
-  neo: async () => {
+  neo: async (): Promise<QueryResponse | ApolloError> => {
     // yyyy-mm-dd
     const today = new Date().toISOString().slice(0, 10);
-    const response = await fetch(
+    const response = (await fetch(
       `https://api.nasa.gov/neo/rest/v1/feed?start_date=${today}&end_date=${today}&api_key=DEMO_KEY`
-    );
-    const jsonResp = await response.json();
+    )) as Response;
+
+    const jsonResp = (await response.json()) as ApiResponse;
+
     const {
-      links,
       element_count,
       near_earth_objects,
       code,
       error,
-      http_error
+      http_error = ''
     } = jsonResp;
 
     if (code && code > 400) {
-      return new ApolloError(http_error, code);
+      return new ApolloError(http_error, code.toString());
     }
     if (error) {
       return new ApolloError(error.message, error.code);
     }
 
+    const objects = near_earth_objects[today]
+      .map(object => ({
+        size: object.estimated_diameter.kilometers.estimated_diameter_min,
+        orbit: object.close_approach_data[0].miss_distance.kilometers
+      }))
+      .map(object => ({
+        size: Math.round(object.size * 100),
+        orbit: +object.orbit
+      }))
+      .sort((a, b) => a.size - b.size)
+      .map((object, i, array) => ({
+        ...object,
+        ...{
+          size: mapSizeRange(
+            object.size,
+            array[0].size,
+            array[array.length - 1].size
+          )
+        }
+      }));
+
     return {
-      links,
-      element_count,
-      near_earth_objects: near_earth_objects[today]
+      elements: element_count,
+      objects
     };
   }
 };
--- server/graphQL/types/nearEarthObjects.ts
+++ server/graphQL/types/nearEarthObjects.ts
@@ -1,63 +1,14 @@
 import { gql } from 'apollo-server-express';
 
 export const NeoTypes = gql`
-  type NeoLinksType {
-    next: String
-    prev: String
-    self: String
-  }
-
-  type NeoDiameterDimensionType {
-    estimated_diameter_min: Float
-    estimated_diameter_max: Float
-  }
-
-  type NeoDiameterType {
-    kilometers: NeoDiameterDimensionType
-    meters: NeoDiameterDimensionType
-    miles: NeoDiameterDimensionType
-    feet: NeoDiameterDimensionType
-  }
-
-  type NeoRelativeVelocityType {
-    kilometers_per_second: Float
-    kilometers_per_hour: Float
-    miles_per_hour: Float
-  }
-
-  type NeoMissDistanceType {
-    astronomical: Float
-    lunar: Float
-    kilometers: Float
-    miles: Float
-  }
-
-  type NeoCloseApproachType {
-    close_approach_date: String
-    close_approach_date_full: String
-    epoch_date_close_approach: Float
-    relative_velocity: NeoRelativeVelocityType
-    miss_distance: NeoMissDistanceType
-    orbiting_body: String
-  }
-
   type NeoObjectsType {
-    links: NeoLinksType
-    id: Float
-    neo_reference_id: Float
-    name: String
-    nasa_jpl_url: String
-    absolute_magnitude_h: Float
-    estimated_diameter: NeoDiameterType
-    is_potentially_hazardous_asteroid: Boolean
-    close_approach_data: [NeoCloseApproachType]
-    is_sentry_object: Boolean
+    size: Float
+    orbit: Float
   }
 
   type NeoOutputType {
-    links: NeoLinksType
-    element_count: Float
-    near_earth_objects: [NeoObjectsType]
+    elements: Float
+    objects: [NeoObjectsType]
   }
 `;
 
