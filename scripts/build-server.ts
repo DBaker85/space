e1703755c0ad119833e5cecc45285157a2d9fd39
David <badcancer22@gmail.com>
2019-09-29 23:55:45 +0200

    rewrite build script

--- scripts/build-server.ts
+++ scripts/build-server.ts
@@ -1,10 +1,17 @@
-import { createProgram, ModuleKind, CompilerOptions } from 'typescript';
+import {
+  createProgram,
+  CompilerOptions,
+  getPreEmitDiagnostics,
+  flattenDiagnosticMessageText
+} from 'typescript';
 import { resolve } from 'path';
 import { readJSON, writeFileSync } from 'fs-extra';
 import chalk from 'chalk';
 
 const entryFile = resolve(__dirname, '..', 'server', 'server.ts');
 const tsConfig = resolve(__dirname, '..', 'server', 'tsconfig.json');
+
+let tsErrors = 0;
 
 const BuildServer = () => {
   console.log('');
@@ -25,6 +32,46 @@ const BuildServer = () => {
 
       let program = createProgram([entryFile], mappedOptions);
       const emitResult = program.emit();
+
+      //--------------------------
+
+      let allDiagnostics = getPreEmitDiagnostics(program).concat(
+        emitResult.diagnostics
+      );
+
+      allDiagnostics.forEach(diagnostic => {
+        if (diagnostic.file) {
+          let {
+            line,
+            character
+          } = diagnostic.file.getLineAndCharacterOfPosition(diagnostic.start!);
+
+          let message = flattenDiagnosticMessageText(
+            diagnostic.messageText,
+            '\n'
+          );
+          console.log(
+            `${chalk.cyan(diagnostic.file.fileName)}:${chalk.yellow(
+              (line + 1).toString()
+            )}:${chalk.yellow((character + 1).toString())} - ${chalk.red(
+              'error'
+            )} ${chalk.gray('TS' + diagnostic.code + ':')} ${message} \n`
+          );
+
+          tsErrors++;
+        } else {
+          console.log(
+            `${flattenDiagnosticMessageText(diagnostic.messageText, '\n')}`
+          );
+          process.exit(1);
+        }
+      });
+
+      if (tsErrors > 0) {
+        console.log(`❌  compiling typescript failed with ${tsErrors} errors
+        `);
+        process.exit(2);
+      }
 
       if (emitResult.emitSkipped) {
         console.log(`❌  ${chalk.red('Error')} compiling typescript
