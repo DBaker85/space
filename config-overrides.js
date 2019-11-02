afb1494e94d36b43c2e31c85b5d14bf403fa44aa
David Baker <dbaker@virtual-affairs.com>
2019-11-02 09:22:14 +0100

    add new webpack flow

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,17 @@
-const { override, adjustWorkbox } = require('customize-cra');
+const { override, adjustWorkbox, addWebpackPlugin } = require('customize-cra');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = override(
   // adjust the underlying workbox
   adjustWorkbox(wb =>
     Object.assign(wb, {
       offlineGoogleAnalytics: true
+    })
+  ),
+  addWebpackPlugin(
+    new HtmlWebpackPlugin({
+      title: 'My App',
+      filename: 'test/admin.json'
     })
   )
 );
