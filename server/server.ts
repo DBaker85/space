addeffb5d6d7133150cea2c58452da08daf8bb25
David <badcancer22@gmail.com>
2019-11-05 12:52:37 +0100

    Final http2 tweaks

--- server/server.ts
+++ server/server.ts
@@ -42,7 +42,9 @@ const h2Options = {
   // Private key
   key: readFileSync(resolve(__dirname, 'keys', 'key.pem')),
   // Fullchain file or cert file (prefer the former)
-  cert: readFileSync(resolve(__dirname, 'keys', 'cert.pem'))
+  cert: readFileSync(resolve(__dirname, 'keys', 'cert.pem')),
+  // TODO: check if we should activate this for dev only
+  allowHTTP1: true
 };
 
 (async () => {
