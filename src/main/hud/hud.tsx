0ddfbe1c958158ab838f1d412a336326bf7e74d2
David Baker <dbaker@virtual-affairs.com>
2019-11-14 13:15:15 +0100

    add hud animation

--- src/main/hud/hud.tsx
+++ src/main/hud/hud.tsx
@@ -1,7 +1,30 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState, useEffect } from 'react';
+import hudImg from '../../assets/images/hud/hud.svg';
 
-const Hud: FunctionComponent = () => {
-  return <div>hud image? hud text</div>;
+interface HudProps {
+  scanDelay: number;
+  targets: number;
+}
+
+const Hud: FunctionComponent<HudProps> = ({ scanDelay, targets }) => {
+  const [scanComplete, setScanComplete] = useState(false);
+
+  setTimeout(() => {
+    setScanComplete(true);
+  }, scanDelay * 1000);
+
+  return (
+    <div>
+      <div>
+        <img src={hudImg} alt="" />
+        <span>
+          {scanComplete
+            ? `found ${targets} target${targets === 1 ? '' : 's'}`
+            : `Scanning Targets`}
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Hud;
