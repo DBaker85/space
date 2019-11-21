2ead8a71ea43ed8dd1e10ba4e03ec9c1b900e874
David Baker <dbaker@virtual-affairs.com>
2019-11-21 16:12:10 +0100

    generate initial gzip files

--- server/utils/getInitialFiles.ts
+++ server/utils/getInitialFiles.ts
@@ -1,22 +1,12 @@
-import {
-  openSync,
-  fstatSync,
-  createReadStream,
-  createWriteStream
-} from 'fs-extra';
+import { openSync, fstatSync } from 'fs-extra';
+
 import { getType } from 'mime';
 import { getExtension } from './getExtension';
 import { ManifestFile } from '../models/models';
-import { createGzip } from 'zlib';
 
 // TODO: is this really the best way to do it?
 export const getInitialFiles = (files: ManifestFile[]) => {
   return files.map(file => {
-    const fileContents = createReadStream(file.filePath);
-    const writeStream = createWriteStream(`${file.filePath}.gz`);
-    const zip = createGzip();
-    fileContents.pipe(zip).pipe(writeStream);
-
     const fd = openSync(`${file.filePath}.gz`, 'r');
     const stat = fstatSync(fd);
 
