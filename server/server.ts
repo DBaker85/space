88aa9813d654e2457d5d45c2831e82163fc55817
David Baker <dbaker@virtual-affairs.com>
2019-11-26 10:45:21 +0100

    downgrade everything

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,4 @@
-import { createSecureServer } from 'http2';
-
-import { resolve } from 'path';
-import { readFileSync } from 'fs-extra';
+import { createServer } from 'http';
 
 import { app } from './app';
 
@@ -9,15 +6,6 @@ const localPort = 5055;
 
 const port = process.env.PORT || localPort;
 
-const h2Options = {
-  // Private key
-  key: readFileSync(resolve(__dirname, 'keys', 'key.pem')),
-  // Fullchain file or cert file (prefer the former)
-  cert: readFileSync(resolve(__dirname, 'keys', 'cert.pem')),
-  // TODO: check if we should activate this for dev only
-  allowHTTP1: true
-};
-
-createSecureServer(h2Options, app.callback()).listen(port, () =>
+createServer(app.callback()).listen(port, () =>
   console.log(`static assets served on ${port}`)
 );
