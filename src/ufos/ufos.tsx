07811cef56c380b011d7c9a81da17c12077e4b2e
David Baker <dbaker@virtual-affairs.com>
2019-11-28 14:24:42 +0100

    style ufos

--- src/ufos/ufos.tsx
+++ src/ufos/ufos.tsx
@@ -10,37 +10,72 @@ import flameIcon from '../assets/images/rocket/fire.svg';
 const Ufos: FunctionComponent = () => {
   const rocketEl = useRef(null);
   const rocketEl2 = useRef(null);
+  const flameEl = useRef(null);
+  const flameEl2 = useRef(null);
+
   useEffect(() => {
-    gsap.to([rocketEl.current as any, rocketEl2.current as any], 5, {
-      bezier: {
-        type: 'soft',
-        values: [
-          {
-            x: () => Math.floor(Math.random() * 10),
-            y: () => -Math.floor(Math.random() * 10)
-          },
-          { x: () => Math.floor(Math.random() * 20), y: 0 },
-          {
-            x: () => Math.floor(Math.random() * 10),
-            y: () => Math.floor(Math.random() * 10)
-          },
-          { x: 0, y: 0 }
-        ],
-        autoRotate: true
-      },
-      ease: 'power0.inOut'
+    // gsap.to([rocketEl.current as any, rocketEl2.current as any], 5, {
+    //   bezier: {
+    //     type: 'soft',
+    //     values: [
+    //       {
+    //         x: () => Math.floor(Math.random() * 10),
+    //         y: () => -Math.floor(Math.random() * 10)
+    //       },
+    //       { x: () => Math.floor(Math.random() * 20), y: 0 },
+    //       {
+    //         x: () => Math.floor(Math.random() * 10),
+    //         y: () => Math.floor(Math.random() * 10)
+    //       },
+    //       { x: 0, y: 0 }
+    //     ],
+    //     autoRotate: true
+    //   },
+    //   ease: 'power0.inOut'
+    // });
+
+    gsap.to(flameEl.current as any, 0.05, {
+      x: '+=2',
+      repeat: -1,
+      yoyo: true
+    });
+
+    gsap.to(flameEl2.current as any, 0.05, {
+      x: '+=2',
+      repeat: -1,
+      yoyo: true
     });
   });
 
   return (
     <div className={styles['ufo-wrapper']}>
-      <div>
-        <img src={rocket2} alt="" ref={rocketEl}></img>
-        <img alt="flame" className={styles['flame']} src={flameIcon} />
+      <div className={`${styles['rocket-holder']} ${styles['type1']}`}>
+        <img
+          src={rocket2}
+          className={styles['rocket']}
+          alt=""
+          ref={rocketEl}
+        ></img>
+        <img
+          alt="flame"
+          className={styles['flame']}
+          ref={flameEl}
+          src={flameIcon}
+        ></img>
       </div>
-      <div>
-        <img src={rocket1} alt="" ref={rocketEl2}></img>
-        <img alt="flame" className={styles['flame']} src={flameIcon} />
+      <div className={`${styles['rocket-holder']} ${styles['type2']}`}>
+        <img
+          src={rocket1}
+          alt=""
+          className={styles['rocket']}
+          ref={rocketEl2}
+        ></img>
+        <img
+          alt="flame"
+          className={styles['flame']}
+          ref={flameEl2}
+          src={flameIcon}
+        />
       </div>
     </div>
   );
