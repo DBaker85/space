321a296e6bf9dcea0047693e658fb0fa537318c8
David Baker <dbaker@virtual-affairs.com>
2019-12-18 17:06:41 +0100

    add comments to compress task

--- scripts/compress-initial-files.ts
+++ scripts/compress-initial-files.ts
@@ -1,22 +1,29 @@
 import { createReadStream, createWriteStream, readJSONSync } from 'fs-extra';
-
 import { resolve } from 'path';
-
-import { ManifestFile, PushManifest } from '../server/models/models';
-
+import chalk from 'chalk';
 import { createGzip } from 'zlib';
+import { ManifestFile, PushManifest } from '../server/models/models';
 
 const fileList: PushManifest = readJSONSync(
   resolve(__dirname, '..', 'build', 'push_manifest.json')
 );
 
+// TODO: Add more verbose logging with filenames and sizes
+
 const compressInitialFiles = (files: ManifestFile[]) => {
-  return files.map(file => {
+  console.log('');
+  console.log(`${chalk.gray('---')} Compressing main bundles ${chalk.gray(
+    '---'
+  )}
+  `);
+  files.forEach(file => {
     const fileContents = createReadStream(file.filePath);
     const writeStream = createWriteStream(`${file.filePath}.gz`);
     const zip = createGzip();
     fileContents.pipe(zip).pipe(writeStream);
   });
+  console.log(`✔️  Initial files compressed
+      `);
 };
 
 compressInitialFiles(fileList.initial);
