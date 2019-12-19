7e44f199e4a71d8818291bba4b0d18a9c48aef29
David Baker <dbaker@virtual-affairs.com>
2019-12-19 12:13:23 +0100

    fix console logs

--- scripts/compress-initial-files.ts
+++ scripts/compress-initial-files.ts
@@ -1,4 +1,10 @@
-import { createReadStream, createWriteStream, readJSONSync } from 'fs-extra';
+import {
+  createReadStream,
+  createWriteStream,
+  readJSONSync,
+  openSync,
+  fstatSync
+} from 'fs-extra';
 import { resolve } from 'path';
 import chalk from 'chalk';
 import { createGzip } from 'zlib';
@@ -8,22 +14,52 @@ const fileList: PushManifest = readJSONSync(
   resolve(__dirname, '..', 'build', 'push_manifest.json')
 );
 
-// TODO: Add more verbose logging with filenames and sizes
+const compressInitialFiles = (files: ManifestFile[]) =>
+  new Promise((resolve, reject) => {
+    console.log('');
+    console.log(`${chalk.gray('---')} Compressing main bundles ${chalk.gray(
+      '---'
+    )}
+  `);
+    const fileCount = files.length;
+    let finishedfiles = [];
+    files.forEach((file, index) => {
+      const fileContents = createReadStream(file.filePath);
+      const writeStream = createWriteStream(`${file.filePath}.gz`);
+      const zip = createGzip();
+      fileContents.pipe(zip).pipe(writeStream);
+      writeStream.on('finish', () => {
+        finishedfiles.push(index);
+        if (finishedfiles.length === fileCount) {
+          resolve();
+        }
+      });
+      writeStream.on('error', () => {
+        reject();
+      });
+    });
+    console.log(`✔️  Initial files compressed
+      `);
+  });
 
-const compressInitialFiles = (files: ManifestFile[]) => {
+const logStats = (files: ManifestFile[]) => {
   console.log('');
-  console.log(`${chalk.gray('---')} Compressing main bundles ${chalk.gray(
-    '---'
-  )}
+  console.log(`${chalk.gray('---')} Gzipped file sizes ${chalk.gray('---')}
   `);
   files.forEach(file => {
-    const fileContents = createReadStream(file.filePath);
-    const writeStream = createWriteStream(`${file.filePath}.gz`);
-    const zip = createGzip();
-    fileContents.pipe(zip).pipe(writeStream);
+    const fd = openSync(`${file.filePath}`, 'r');
+    const stat = fstatSync(fd);
+    const zipFd = openSync(`${file.filePath}.gz`, 'r');
+    const zipStat = fstatSync(zipFd);
+    const filePath = file.filePath.split('/');
+    const filename = filePath.pop();
+    console.log(
+      `${(stat.size / 1024).toFixed(2)} Kb \t> \t${chalk.green(
+        (zipStat.size / 1024).toFixed(2)
+      )} Kb`,
+      `\t ${filePath.join('/')}/${chalk.cyan(filename)}.gz`
+    );
   });
-  console.log(`✔️  Initial files compressed
-      `);
 };
 
-compressInitialFiles(fileList.initial);
+compressInitialFiles(fileList.initial).then(() => logStats(fileList.initial));
