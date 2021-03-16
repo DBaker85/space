a38bda2803c0ae8c44d6504b0cd4cd17e68302d0
David <badcancer22@gmail.com>
2021-03-16 11:37:30 +0100

    fix hmr

--- server/server.ts
+++ server/server.ts
@@ -1,42 +1,30 @@
-import { createServer } from 'http';
-
+import { createServer } from "http";
+const destroyable = require("server-destroy");
 // import { app } from './app';
+declare const module: any;
 
+const Koa = require("koa");
+const app = new Koa();
 const localPort = 5055;
 
 const port = process.env.PORT || localPort;
 
-// createServer(app.callback()).listen(port, () =>
-//   console.log(`static assets served on ${port}`)
-// );
+const server = createServer(app.callback()).listen(port, () =>
+  console.log(`static assets served on ${port}`)
+) as any;
+destroyable(server);
 
-const Koa = require('koa');
-const app = new Koa();
-
-app.use(async (ctx:any) => {
-  ctx.body = 'Hello World part 10';
+app.use(async (ctx: any) => {
+  ctx.body = "Hello there again, yup";
 });
 
+if (process.env.NODE_ENV === "development") {
+  // Here HMR story begins
 
-const _server = app.listen(port);
-
-
-// Here HMR story begins
-declare const module: any;
-// You need only 3 lines of code to start accepting code changes coming through the HMR
-if (module.hot) {
+  // You need only 3 lines of code to start accepting code changes coming through the HMR
+  if (module.hot) {
     module.hot.accept();
     // Next callback is essential: After code changes were accepted     we need to restart the app. server.close() is here Express.JS-specific and can differ in other frameworks. The idea is that you should shut down your app here. Data/state saving between shutdown and new start is possible
-    module.hot.dispose(()=>_server.close());
+    module.hot.dispose(() => server.destroy());
+  }
 }
-
-
-// var destroyable = require('server-destroy');
-// var http = require('http');
-
-// var server = http.createServer(app.callback());
-// server.listen();
-// destroyable(server);
-
-// // then
-// server.destroy();
