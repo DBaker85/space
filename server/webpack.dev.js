0f5cf707ce45b0f7ef4f25684181d0d12e35ace9
David Baker <davidbaker@MacBook-Pro.local>
2021-03-19 15:48:58 +0100

    fix build tasks

--- server/webpack.dev.js
+++ server/webpack.dev.js
@@ -2,54 +2,54 @@
 
 const webpack = require("webpack");
 const { merge } = require("webpack-merge");
-const nodeExternals = require('webpack-node-externals');
+const nodeExternals = require("webpack-node-externals");
 const { resolve } = require("path");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const StartServerPlugin = require('start-server-nestjs-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const StartServerPlugin = require("start-server-nestjs-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   context: resolve(__dirname),
-  entry: [   
-    'webpack/hot/poll?1000',// bundle the client for hot reloading, only- means to only hot reload for successful updates
+  entry: [
+    "webpack/hot/poll?1000", // bundle the client for hot reloading, only- means to only hot reload for successful updates
     "./server.tsx",
   ],
   watch: true,
   mode: "development",
   output: {
-    path: resolve(__dirname,  'dist'),
-    filename: 'server.js',
+    path: resolve(__dirname, "dist"),
+    filename: "server.js",
     // publicPath: '/assets/',
-    libraryTarget: 'commonjs2',
-    
-},
+    libraryTarget: "commonjs2",
+  },
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx"],
   },
-  target: 'node',
-  node:{
-    __dirname: false
-  }, 
-  externals: [nodeExternals({
-    allowlist: ['webpack/hot/poll?1000']
-})],
+  target: "node",
+  node: {
+    __dirname: false,
+  },
+  externals: [
+    nodeExternals({
+      allowlist: ["webpack/hot/poll?1000"],
+    }),
+  ],
   module: {
     rules: [
       {
         test: [/\.jsx?$/, /\.tsx?$/],
-        use: {loader: "babel-loader",
-        options: {
-          presets: ["@babel/preset-typescript","@babel/preset-react"]
-        }},
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-typescript", "@babel/preset-react"],
+          },
+        },
         exclude: /node_modules/,
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
-   
-
-     
     ],
   },
   devServer: {
@@ -60,13 +60,13 @@ module.exports = {
     new CopyPlugin({
       patterns: [
         {
-          from: resolve(process.cwd(), '..', "client", "build"),
-          to: resolve(__dirname,  'dist', 'public'),
+          from: resolve(process.cwd(), "..", "client", "dist"),
+          to: resolve(__dirname, "dist", "public"),
         },
       ],
     }),
     new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: ['**/*', '!public*']
+      cleanOnceBeforeBuildPatterns: ["**/*", "!public*"],
     }),
     new webpack.HotModuleReplacementPlugin(), // enable HMR globally
     new webpack.DefinePlugin({
@@ -74,7 +74,7 @@ module.exports = {
         NODE_ENV: JSON.stringify("development"),
       },
     }),
-    new StartServerPlugin({name:'server.js', nodeArgs: ['--inspect']}),
+    new StartServerPlugin({ name: "server.js", nodeArgs: ["--inspect"] }),
   ],
 
   performance: {
